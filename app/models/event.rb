f7d68b26ef1bca1f94e34bbb28fe373967b1898f
Web <vjlucero@miuandes.cl>
2018-04-03 14:20:58 -0300

    created validation of start_date of event. Started creating validation on equal events at same date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,19 @@
 class Event < ApplicationRecord
   belongs_to :place
   has_many :tickets
+  validate :start_date_after_creation
+  validate :is_posible_to_create
+  def start_date_after_creation
+    if DateTime.now.to_date >= start_date
+      errors.add(:start_date, 'has to be after creation time')
+    end
+  end
+  def is_posible_to_create
+    if place_id
+      if start_date
+        errors.add()
+      end
+    end
+  end
+
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,5 +5,5 @@ class Ticket < ApplicationRecord
 
   validates :price, presence:true,
             numericality: {greater_than_or_equal_to: 0}
-  
+
 end
